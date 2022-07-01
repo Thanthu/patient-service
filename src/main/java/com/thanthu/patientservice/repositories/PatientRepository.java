f1e9a8cf8893e684de565c442010f8e7bb887856
Thanthu <tnair@orthofx.com>
2022-07-01 17:56:50 +0530

    create repository

---
+++ src/main/java/com/thanthu/patientservice/repositories/PatientRepository.java
@@ -0,0 +1,16 @@
+package com.thanthu.patientservice.repositories;
+
+import java.util.List;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+
+import com.thanthu.patientservice.models.Patient;
+
+public interface PatientRepository extends JpaRepository<Patient, Long>  {
+
+	@Query("select p from Patient p where lower(concat(p.firstName, ' ', p.lastName)) like %:name%")
+	public List<Patient> findAllByName(@Param("name") String name);
+
+}
