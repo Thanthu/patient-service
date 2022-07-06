bdef36b1b7ecb2cf3fa0f529b577fbdbc7137511
Thanthu <tnair@orthofx.com>
2022-07-06 10:44:22 +0530

    update patient with email, password and roles update patientdto for the same update patient and patientdto converters for the same changes

--- src/main/java/com/thanthu/patientservice/models/Patient.java
+++ src/main/java/com/thanthu/patientservice/models/Patient.java
@@ -2,12 +2,18 @@ package com.thanthu.patientservice.models;
 
 import java.time.LocalDate;
 import java.time.LocalDateTime;
+import java.util.HashSet;
+import java.util.Set;
 
 import javax.persistence.Column;
 import javax.persistence.Entity;
+import javax.persistence.FetchType;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.JoinTable;
+import javax.persistence.ManyToMany;
 import javax.persistence.Table;
 import javax.validation.constraints.NotNull;
 
@@ -35,8 +41,14 @@ public class Patient {
 	
 	@EqualsAndHashCode.Include
 	@Id
-	@GeneratedValue(strategy = GenerationType.AUTO)
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Long id;
+	
+	@CreationTimestamp
+	private LocalDateTime createdDateTime;
+	
+	@UpdateTimestamp
+	private LocalDateTime updateDateTime;
 	
 	@NotNull
 	@Column(nullable = false)
@@ -50,10 +62,18 @@ public class Patient {
 	@Column(nullable = false)
 	private LocalDate dob;
 	
-	@CreationTimestamp
-	private LocalDateTime createdDateTime;
+	@NotNull
+	@Column(nullable = false, unique = true)
+	private String email;
 	
-	@UpdateTimestamp
-	private LocalDateTime updateDateTime;
-
+	@NotNull
+	@Column(nullable = false, length = 500)
+	private String password;
+	
+	@ToString.Exclude
+	@Builder.Default
+	@ManyToMany(fetch = FetchType.EAGER)
+	@JoinTable(name = "patient_roles", joinColumns = @JoinColumn(name = "patient_id"), inverseJoinColumns = @JoinColumn(name = "role_id"))
+	private Set<Role> roles = new HashSet<Role>();
+	
 }
