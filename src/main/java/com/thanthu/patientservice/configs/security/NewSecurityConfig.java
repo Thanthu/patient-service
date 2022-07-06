b5c7fb5026e5f0dc075fd31c99a89b944f7cfe3a
Thanthu <tnair@orthofx.com>
2022-07-06 15:04:39 +0530

    change deprecated WebSecurityConfigurerAdapter implementation of security config

--- src/main/java/com/thanthu/patientservice/configs/security/NewSecurityConfig.java
+++ src/main/java/com/thanthu/patientservice/configs/security/NewSecurityConfig.java
@@ -3,44 +3,30 @@ package com.thanthu.patientservice.configs.security;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpMethod;
-import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
 
 import com.thanthu.patientservice.enums.RoleName;
 
-import lombok.RequiredArgsConstructor;
-
-// Not deleting this calls for future reference. Changes to avoid deprecation done in @NewSecurityConfig
-@RequiredArgsConstructor
-//@EnableWebSecurity
-//@Configuration
-public class SecurityConfig extends WebSecurityConfigurerAdapter {
+@Configuration
+public class NewSecurityConfig {
 	
-	private final UserDetailsServiceImpl userDetailsService;
-	
-	@Override
-	protected void configure(AuthenticationManagerBuilder auth) throws Exception {
-		auth.userDetailsService(userDetailsService);
-	}
-	
-	@Override
-	protected void configure(HttpSecurity http) throws Exception {
+	@Bean
+	public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
 		http.headers().frameOptions().sameOrigin();
 		http.csrf().disable();
 		http.httpBasic();
 		http.authorizeRequests()
 		.mvcMatchers(HttpMethod.POST, "/api/v1/patient").hasAuthority(RoleName.CREATE_PATIENT.name())
 		.mvcMatchers("/api/v1/patient/**").hasAuthority(RoleName.PATIENT.name());
-	}
+        return http.build();
+    }
 	
 	@Bean
 	public PasswordEncoder passwordEncoder() {
 		return new BCryptPasswordEncoder();
 	}
-	
 
 }
