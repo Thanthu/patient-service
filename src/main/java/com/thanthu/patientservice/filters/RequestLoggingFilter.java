2d317bf749a8ef272ce15bc066fa06213328e856
Thanthu <tnair@orthofx.com>
2022-07-15 23:44:28 +0530

    create CommonsRequestLoggingFilter Bean

---
+++ src/main/java/com/thanthu/patientservice/filters/RequestLoggingFilter.java
@@ -0,0 +1,20 @@
+package com.thanthu.patientservice.filters;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.filter.CommonsRequestLoggingFilter;
+
+@Configuration
+public class RequestLoggingFilter {
+
+	@Bean
+	public CommonsRequestLoggingFilter commonsRequestLoggingFilter() {
+		CommonsRequestLoggingFilter loggingFilter = new CommonsRequestLoggingFilter();
+		loggingFilter.setIncludeClientInfo(true);
+		loggingFilter.setIncludeQueryString(true);
+		loggingFilter.setIncludePayload(true);
+		loggingFilter.setMaxPayloadLength(64000);
+		return loggingFilter;
+	}
+
+}
